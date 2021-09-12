7d0c6c5140c55b981ca46babd2f44823408e752d
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2021-09-12 19:09:52 +0200

    showing article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_08_185118) do
+ActiveRecord::Schema.define(version: 2021_09_08_185727) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
