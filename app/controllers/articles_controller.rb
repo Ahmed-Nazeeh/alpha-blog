819d35209d5df83858b290b909c0f8bf74b2d6d8
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2021-09-15 09:15:26 +0200

    validation and flash messages , update existing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,36 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def create
+        #render plain: params[:article] # output of this code =>{"title"=>"123", "description"=>"456789"}
+        #@article = Article.new(params[:article])
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect  #test
+        if @article.save
+            flash[:notice] = "Article was created successfully"
+             redirect_to article_path(@article) # or redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        #byebug
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfuly"
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
 end
