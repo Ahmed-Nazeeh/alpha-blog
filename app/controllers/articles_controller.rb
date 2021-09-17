8eb212a2a587082dcaa80a0549cb01f5eeae3d15
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2021-09-17 09:52:55 +0200

    DRY CODE - Dont repeat yourself

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only:[:show, :edit, :update , :destroy]
 
     def show
         #byebug
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
 
     def index
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
     def create
         #render plain: params[:article] # output of this code =>{"title"=>"123", "description"=>"456789"}
         #@article = Article.new(params[:article])
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         #render plain: @article.inspect  #test
         if @article.save
             flash[:notice] = "Article was created successfully"
@@ -28,16 +29,32 @@ class ArticlesController < ApplicationController
 
     def edit
         #byebug
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        #@article = Article.find(params[:id])
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfuly"
             redirect_to @article
         else
             render 'edit'
         end
+    end
+
+    def destroy
+        #@article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
