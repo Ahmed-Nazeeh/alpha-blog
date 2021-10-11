7e7188fb617c2c8452ad4c0a0eb457cf37c41815
Ahmed-Nazeeh <89453140+Ahmed-Nazeeh@users.noreply.github.com>
2021-10-11 17:23:25 +0200

    Add edit tamplate and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def create
         #byebug
         @user = User.new(user_params)
